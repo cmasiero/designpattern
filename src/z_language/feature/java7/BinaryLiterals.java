91101e4467a490b04c8f9678b9f0fef24dace76b
Cristiano Masiero <c_masiero@yahoo.it>
2017-05-06 19:13:51 +0200

    new java features begin

---
+++ src/z_language/feature/java7/BinaryLiterals.java
@@ -0,0 +1,13 @@
+package z_language.feature.java7;
+
+/**
+ * Created by cristiano on 06/05/17.
+ */
+public class BinaryLiterals {
+
+    public static void main(String[] args) {
+        // Todo : BinaryLiterals
+        System.out.println("BinaryLiterals.main todo ...");
+    }
+
+}
---
+++ src/z_language/feature/java7/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Created by cristiano on 06/05/17.
+ *
+ * New languages features java 7
+ *
+ */
+package z_language.feature.java7;
